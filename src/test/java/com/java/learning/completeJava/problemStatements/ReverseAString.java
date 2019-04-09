a1a8ace557067c9f4354ec57789108167e5704ca
Vinay Raghumanda <VRaghumanda@altimetrik.com>
2019-04-09 18:46:38 +0530

    adding problem statements

---
+++ src/test/java/com/java/learning/completeJava/problemStatements/ReverseAString.java
@@ -0,0 +1,39 @@
+package com.java.learning.completeJava.problemStatements;
+
+import java.util.Scanner;
+
+import org.testng.annotations.Test;
+
+public class ReverseAString {
+	
+	@Test
+	public void usingStringBuilder() {
+		String s = "Vinay";
+		StringBuilder sb = new StringBuilder();
+		sb.append(s);
+		System.out.println(sb.reverse());
+	}
+
+	@Test
+	public void usingCharAt() {
+		
+		String s = "vinay";
+		System.out.println("Reversed String is ");
+		for(int i=s.length()-1; i>=0; i--) {
+			System.out.print(s.charAt(i));
+		}		
+	}
+	
+	@Test
+	public void usingSplit() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter string");
+		String input = sc.nextLine();
+		String[] token = input.split("");
+		System.out.println("Reversed String is ");
+		for(int i = token.length-1; i>=0; i--) {
+			System.out.print(token[i]);
+		}
+	}
+
+}
---
+++ src/test/java/com/java/learning/completeJava/problemStatements/Swap.java
@@ -0,0 +1,31 @@
+package com.java.learning.completeJava.problemStatements;
+
+import org.testng.annotations.Test;
+
+public class Swap {
+
+	@Test
+	public void usingThirdVariable() {
+
+		int a = 10, b = 20, temp;
+		
+		temp = a;
+		a = b;
+		b = temp;
+
+		System.out.print("a " + a + " b " + b);
+
+	}
+	
+	@Test
+	public void withoutUsingThirdVariable() {
+		int a=10, b=20;
+		
+		a=a+b;//30
+		b=a-b;//30-20=10
+		a=a-b;//30-10=20
+		
+		System.out.print("a " + a + " b " + b);
+	}
+
+}
