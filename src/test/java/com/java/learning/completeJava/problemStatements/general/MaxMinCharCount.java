13688a46259d27a0e233ceb01d8b0685cc3ead81
Vinay Raghumanda <vinay.raghu10@gmail.com>
2022-07-10 10:43:12 +0530

    max and min occurence of characters in string

--- src/test/java/com/java/learning/completeJava/problemStatements/general/MaxMinCharCount.java
+++ src/test/java/com/java/learning/completeJava/problemStatements/general/MaxMinCharCount.java
@@ -2,7 +2,7 @@ package com.java.learning.completeJava.problemStatements.general;
 
 import java.util.Scanner;
 
-public class MaxCharCount {
+public class MaxMinCharCount {
     public static void main(String[] args) {
         System.out.println("Enter a String");
         String input = new Scanner(System.in).next();
@@ -11,16 +11,23 @@ public class MaxCharCount {
             count[input.charAt(i)]++;
         }
 
-        int max = -1;
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
         char maxResult = ' ';
+        char minResult = ' ';
 
         for(int i=0; i<input.length(); i++){
             if(max<count[input.charAt(i)]){
                 max = count[input.charAt(i)];
                 maxResult = input.charAt(i);
             }
+            if(min>count[input.charAt(i)]){
+                min = count[input.charAt(i)];
+                minResult = input.charAt(i);
+            }
         }
 
         System.out.println("Maximum character count is "+max+" for character "+maxResult);
+        System.out.println("Maximum character count is "+min+" for character "+minResult);
     }
 }
